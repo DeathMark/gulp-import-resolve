fc36f1a6560b767935d50f3e652cad4b5e3d15df
Tuan Pham <tuanpm@skylinesoft.vn>
2017-04-24 16:53:21 +0700

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var fs			= require('fs');
+var path		= require('path');
+var gutil		= require('gulp-util');
+var throught	= require('through2');
+
+function importResolve(content, baseDir) {
+	var regex = /@import[^'"]+?['"](.+?)['"];?/g;
+
+	if (regex.test(content)) {
+		content	= content.replace(regex, function(m, capture) {
+			var parse	= path.parse(path.resolve(baseDir, capture));
+			var file	= parse.dir + '/_' + parse.name;
+
+			if (fs.existsSync(file + '.scss')) {
+				file	= file + '.scss';
+			} else if (fs.existsSync(file + '.scss.liquid')) {
+				file	= file + '.scss.liquid';
+			} else {
+				return '';
+			}
+
+			var file_contents	= fs.readFileSync(file);
+
+			return file_contents;
+		});
+	}
+
+	return content;
+}
+
+
+
+module.exports	= function(opts) {
+	return throught.obj(function(file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-import-resovle', 'Streaming not supported'));
+			return;
+		}
+
+		if (path.basename(file.path).indexOf('_') === 0) {
+			return cb();
+		}
+
+		file.contents	= new Buffer(importResolve(file.contents.toString(), path.dirname(file.path)));
+
+		setImmediate(cb, null, file);
+	});
+};
